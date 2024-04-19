02cbbde268cd04a36b89421cf0e13f8742e1d033
XhoxhoT <xhonitinaj@yahoo.com>
2024-04-19 18:52:05 +0200

    configuring loging

--- src/main/java/com/hendisantika/springbootrestapipostgresql/controller/AuthorRestController.java
+++ src/main/java/com/hendisantika/springbootrestapipostgresql/controller/AuthorRestController.java
@@ -1,28 +1,19 @@
 package com.hendisantika.springbootrestapipostgresql.controller;
 
-
 import com.hendisantika.springbootrestapipostgresql.entity.Author;
-import com.hendisantika.springbootrestapipostgresql.entity.Book;
 import com.hendisantika.springbootrestapipostgresql.repository.AuthorRepository;
-import com.hendisantika.springbootrestapipostgresql.repository.BookRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/author")
+@Slf4j // Import log object from Lombok
 public class AuthorRestController {
 
     @Autowired
@@ -30,43 +21,48 @@ public class AuthorRestController {
 
     @PostMapping
     public ResponseEntity<?> addAuthor(@RequestBody Author author) {
+        log.info("Adding new author: {}", author);
         return new ResponseEntity<>(repository.save(author), HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<Collection<Author>> getAllAuthors() {
+        log.info("Retrieving all authors");
         return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Author> getAuthorWithId(@PathVariable Long id) {
+        log.info("Retrieving author with ID: {}", id);
         return new ResponseEntity<Author>(repository.findById(id).get(), HttpStatus.OK);
     }
 
     @GetMapping(params = {"name"})
     public ResponseEntity<Collection<Author>> findBookWithName(@RequestParam(value = "name") String name) {
+        log.info("Searching author with name: {}", name);
         return new ResponseEntity<>(repository.findByName(name), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Author> updateAuthorFromDB(@PathVariable("id") long id, @RequestBody Author author) {
-
+        log.info("Updating author with ID: {}", id);
         Optional<Author> currentAuthorOpt = repository.findById(id);
         Author currentAuthor = currentAuthorOpt.get();
         currentAuthor.setName(author.getName());
         currentAuthor.setISBN(author.getISBN());
         currentAuthor.setSurname(author.getSurname());
-
         return new ResponseEntity<>(repository.save(currentAuthor), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public void deleteAuthorWithId(@PathVariable Long id) {
+        log.info("Deleting author with ID: {}", id);
         repository.deleteById(id);
     }
 
     @DeleteMapping
     public void deleteAllAuthors() {
+        log.info("Deleting all authors");
         repository.deleteAll();
     }
 }
--- src/main/java/com/hendisantika/springbootrestapipostgresql/controller/BookRestController.java
+++ src/main/java/com/hendisantika/springbootrestapipostgresql/controller/BookRestController.java
@@ -2,34 +2,18 @@ package com.hendisantika.springbootrestapipostgresql.controller;
 
 import com.hendisantika.springbootrestapipostgresql.entity.Book;
 import com.hendisantika.springbootrestapipostgresql.repository.BookRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.Optional;
 
-/**
- * Created by IntelliJ IDEA.
- * Project : spring-boot-rest-api-postgresql
- * User: hendisantika
- * Email: hendisantika@gmail.com
- * Telegram : @hendisantika34
- * Date: 2019-01-18
- * Time: 22:07
- * To change this template use File | Settings | File Templates.
- */
 @RestController
 @RequestMapping("/api/books")
+@Slf4j // Import log object from Lombok
 public class BookRestController {
 
     @Autowired
@@ -37,43 +21,48 @@ public class BookRestController {
 
     @PostMapping
     public ResponseEntity<?> addBook(@RequestBody Book book) {
+        log.info("Adding new book: {}", book);
         return new ResponseEntity<>(repository.save(book), HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<Collection<Book>> getAllBooks() {
+        log.info("Retrieving all books");
         return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Book> getBookWithId(@PathVariable Long id) {
+        log.info("Retrieving book with ID: {}", id);
         return new ResponseEntity<Book>(repository.findById(id).get(), HttpStatus.OK);
     }
 
     @GetMapping(params = {"name"})
     public ResponseEntity<Collection<Book>> findBookWithName(@RequestParam(value = "name") String name) {
+        log.info("Searching book with name: {}", name);
         return new ResponseEntity<>(repository.findByName(name), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Book> updateBookFromDB(@PathVariable("id") long id, @RequestBody Book book) {
-
+        log.info("Updating book with ID: {}", id);
         Optional<Book> currentBookOpt = repository.findById(id);
         Book currentBook = currentBookOpt.get();
         currentBook.setName(book.getName());
         currentBook.setDescription(book.getDescription());
         currentBook.setTags(book.getTags());
-
         return new ResponseEntity<>(repository.save(currentBook), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public void deleteBookWithId(@PathVariable Long id) {
+        log.info("Deleting book with ID: {}", id);
         repository.deleteById(id);
     }
 
     @DeleteMapping
     public void deleteAllBooks() {
+        log.info("Deleting all books");
         repository.deleteAll();
     }
 }
